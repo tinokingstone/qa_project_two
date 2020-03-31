8b84580f5188b8d2729157d93778a2efcbbbd83e
Ubuntu <tinokingstone@plug.g5ra0pgrhs2udkewg2yycuepnh.fx.internal.cloudapp.net>
2020-03-31 01:17:08 +0000

    service4 updated ip4

--- service4/app.py
+++ service4/app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 def service4():
      
-  rand_str = requests.get('http://localhost:5002/')
-  rand_int = requests.get('http://localhost:5001/')  
+  rand_str = requests.get('http://51.140.240.158:5002/')
+  rand_int = requests.get('http://51.140.240.158:5001/')  
   str_int = str(rand_str.text) + str(rand_int.text) 
 
   return str(str_int) #,  {"rand_int":rand_int} ,"asdasdasdasdad"
