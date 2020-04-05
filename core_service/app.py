20871f755275b529e2611460e8b157b732c9eaf7
tinokingstone <tinokingstone@gmail.com>
2020-04-05 21:37:56 +0100

    succsessfully merged mvp files with game app files

--- core_service/app.py
+++ core_service/app.py
@@ -1,18 +1,6 @@
-from flask import Flask, Request, Response
-import requests
-import random
-from os import getenv
+from application import app 
 
 
-app = Flask(__name__)
-
-@app.route('/', methods=['GET','POST'])
-def core_service():
-
-  rand_str = requests.get('http://51.140.240.158:5003/')
-  final_res = str(rand_str.text)
-
-  return "the final random number and string put together are " + str(final_res) #,  {"rand_int":rand_int} ,"asdasdasdasdad"
 if __name__=='__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
 
