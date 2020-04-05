3034de94d33d9d59b0ce44df2a40011865ee705f
tinokingstone <tinokingstone@gmail.com>
2020-04-05 21:23:51 +0100

    master wiped ready for clean up

--- core_service/application/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
-from wtforms import SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
-class Rand_obj_f(FlaskForm):
-
-	submit = SubmitField('Play')
--- rand_stats/Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM python:3.7
-#RUN apt-get update -y
-WORKDIR /app
-COPY ./requirements.txt .
-RUN pip3 install -r requirements.txt
-COPY . .
-ENTRYPOINT ["/usr/local/bin/python","app.py"]
